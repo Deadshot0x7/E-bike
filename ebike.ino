fdd199d78b8271f0dfb39a3454dbf3b8865800a5
DeadShot0x7 <68690437+Deadshot0x7@users.noreply.github.com>
2022-06-09 21:42:37 +0530

    Added

--- ebike.ino
+++ ebike.ino
@@ -2,6 +2,7 @@ int gyro_first =3;
 int gyro_second= 4;
 int tilt_sensor=5;
 void setup() {
+  Serial.begain(9600);
   pin.Mode(gyro_first,Input);
   pin.Mode(gyro_second,Input);
   pin.Mode(tilt_sensor,Input);
@@ -9,7 +10,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+Serial.print("Hello World I'm inside Loop function");
 
 
 }
