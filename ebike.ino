30f3c5c30516a4ae2c1ad3c0ba32793229d073be
DeadShot0x7 <68690437+Deadshot0x7@users.noreply.github.com>
2022-06-10 09:41:21 +0530

    Update ebike.ino

--- ebike.ino
+++ ebike.ino
@@ -1,11 +1,13 @@
 int gyro_first =3;
 int gyro_second= 4;
 int tilt_sensor=5;
+int relay_pin=6;
 void setup() {
   Serial.begain(9600);
   pin.Mode(gyro_first,Input);
   pin.Mode(gyro_second,Input);
   pin.Mode(tilt_sensor,Input);
+  pin.Mode(relay_pin,Output);
 }
 
 void loop() {
