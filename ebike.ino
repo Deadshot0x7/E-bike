f89c38dcba4cf0d0ebb47fef3e70814a8ca5d11f
DeadShot0x7 <68690437+Deadshot0x7@users.noreply.github.com>
2022-06-10 12:39:27 +0530

    Added
    Added the gyroscope code and Selected the axis

--- ebike.ino
+++ ebike.ino
@@ -1,18 +1,18 @@
-int gyro_first =3;
-int gyro_second= 4;
-int tilt_sensor=5;
-int relay_pin=6;
-void setup() {
-  Serial.begain(9600);
-  pin.Mode(gyro_first,Input);
-  pin.Mode(gyro_second,Input);
-  pin.Mode(tilt_sensor,Input);
-  pin.Mode(relay_pin,Output);
-}
+#include<Wire.h>
 
-void loop() {
-  // put your main code here, to run repeatedly:
-Serial.print("Hello World I'm inside Loop function");
+const int MPU_addr=0x68; int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 
+int minVal=265; int maxVal=402;
 
+double x; double y; double z;
+
+void setup(){ Wire.begin(); Wire.beginTransmission(MPU_addr); Wire.write(0x6B); Wire.write(0); Wire.endTransmission(true); Serial.begin(9600); } void loop(){ Wire.beginTransmission(MPU_addr); Wire.write(0x3B); Wire.endTransmission(false); Wire.requestFrom(MPU_addr,14,true); AcX=Wire.read()<<8|Wire.read(); AcY=Wire.read()<<8|Wire.read(); AcZ=Wire.read()<<8|Wire.read(); int xAng = map(AcX,minVal,maxVal,-90,90); int yAng = map(AcY,minVal,maxVal,-90,90); int zAng = map(AcZ,minVal,maxVal,-90,90);
+
+x= RAD_TO_DEG * (atan2(-yAng, -zAng)+PI); y= RAD_TO_DEG * (atan2(-xAng, -zAng)+PI); z= RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
+
+Serial.print("AngleX= "); Serial.println(x);
+
+Serial.print("AngleY= "); Serial.println(y);
+
+Serial.print("AngleZ= "); Serial.println(z); Serial.println("-----------------------------------------"); delay(400);
 }
